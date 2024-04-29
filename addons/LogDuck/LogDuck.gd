f9b1b07e88061a5d6ba6a8b540af04a07d8e8510
Zee Weasel <theweasel86@gmail.com>
2024-04-29 17:32:35 -0400

    0.7.1 Settings File, Version Name, Register Class Names
    - Moved Settings to LogDuckSettings.gd, making it easier to diff when updating the plugin.
    - Fixed incorrect version number in plugin.cfg
    - Added Icon To Test_Scene
    - Added new function register_class_name(_class_name), which can be called from
    - Added verbose boolean to enable / disable LogDuck console output

--- addons/LogDuck/LogDuck.gd
+++ addons/LogDuck/LogDuck.gd
@@ -1,134 +1,18 @@
-extends Node
-const VERSION = "v0.7"
+extends LogDuckSettings
+const VERSION = "v0.7.1"
 
 ## Use LogDuck.d(), .e(), or .w() for logging debug, error, or warning messages.
 ## Supports up to 6 arguments (or 7, if not starting with a string message).
 ## Ensure LogDuck is high in the autoload script order to avoid null errors.
 
-## Show the stack frame that called the log in Debug messages.
-var show_stack_frame_debug = false
+###########################################
+## Adjust Settings in LogDuckSettings.gd ##
+###########################################
 
-## Show the entire stack frame that called the Log in Debug messages.
-var show_full_stack_debug = false
-
-## Show the stack frame that called the log in Warning messages.
-var show_stack_frame_warning = true
-
-## Show the entire stack frame that called the Log in Warning messages.
-var show_full_stack_warning = false
-
-## Show the stack frame that called the log in Error messages.
-var show_stack_frame_error = true
-
-## Show the entire stack frame that called the Log in Error messages.
-var show_full_stack_error = false
-
-
-## The text-prefix to be added to the beginning of Error messages
-var prefix_error = "[ERROR] "
-## The text-prefix to be added to the beginning of Warning messages
-var prefix_warning = "[WARNING] "
-## The text-prefix to be added to the beginning of Debug messages
-var preface_debug = ""
-
-
-## The output format of (plain) console messages
-## Order of placeholders: Prefix, Class Name, Message/Argument, Extra Arguments
-var console_output_format = "%s(%s) %s %s"
-
-## The format of the stack frame line when enabled.
-## Order of placeholders: Script path, line number, function
-var console_format_stack_frame = "%s:%s -> %s"
-
-## The format of the stack frame line when enabled.
-## Stack is output as a single string
-var console_format_full_stack = "%s"
-
-## Show Debug messages in the console
-var enable_console_debug := true
-
-## Show warnings in the console
-var enable_console_warnings := true
-
-## Push warnings to the Godot Debugger
-## (Only works in editor)
-var enable_debugger_warnings := true
-
-## Show errors in the console
-var enable_console_errors := true
-
-## Push errors to the Godot Debugger
-## (Only works in editor)
-var enable_errors_in_debugger := true
-
-## Enable / disable asserts to pause the game when an error gets logged
-## Note: Only works in editor or debug builds
-var throw_exception_on_error := false
-
-#var SHOW_TIME : bool = true
-#var SHOW_SECONDS_AFTER_LAUNCH : bool = true
-
-## Select, which type of seperator to use for the arguments
-var seperator : SeperatorType = SeperatorType.COMMA
-
-
-## Settings to customize print_rich() output. Please note, that if you remove
-## a %s-placeholder, you will need to remove the arguments when formatting the string.
-
-## Enable to customize your ouput with BBCode
-var print_rich : bool = true
-
-## Set true if you don't want to parse BB Code sent in arguments
-## Can be useful for chat messages, or if you output RichLabelText content
-var escape_bbcode_in_arguments : bool = true
-
-
-## Format to be applied to rich console output for stack frame
-var rich_console_format_stack_frame = "[i]%s:%s -> %s[/i]"
-
-## Set true if you don't want to parse BB Code sent in arguments
-## Can be useful for chat messages, or if you output RichLabelText content
-var rich_console_format_full_stack = "[i]%s[/i]"
-
-# (1) Prefix (see above) (2) Class Name
-var rich_prefix_error = "[b]%s[/b] [b](%s)[/b] " # Uses prefix_error for 1st %s
-var rich_msg_error = "%s"
-var rich_args_error = "[i]%s[/i]"
-
-var rich_prefix_warning = "[b]%s[/b] [b](%s)[/b] " # Uses prefix_warning for 1st %s
-var rich_msg_warning = "%s"
-var rich_args_warning = "[i]%s[/i]"
-
-var rich_preface_debug = "[b]%s[/b][b](%s)[/b] " # Uses preface_debug for 1st %s
-var rich_msg_debug = "%s"
-var rich_args_debug = "[i]%s[/i]"
-
-# Add BBCode tags that will apply to the entire output message
-var rich_format_debug = "[color=white]%s%s %s[/color]" % [rich_preface_debug, rich_msg_debug, rich_args_debug]
-var rich_format_warn = "[color=yellow]%s%s %s[/color]" % [rich_prefix_warning, rich_msg_warning, rich_args_warning]
-var rich_format_error = "[color=red]%s%s %s[/color]" % [rich_prefix_error, rich_msg_error, rich_args_error]
-
-
-#################################
-## Only Monsters Lurking Below ##
-#################################
-
-
-## Used to identify the log level of the message throughout the script
-enum LogLevel {
-	DEBUG,
-	WARN,
-	ERROR,
-}
-
-enum SeperatorType {
-	SPACE,
-	COMMA,
-	PIPE, # |
-}
 
 ## The Dictionary holding holding class names with their script path as key
-## Is tried to be filled automatically by LogDuck, but entries can be manually added
+## Is tried to be filled automatically by LogDuck, but entries can be manually 
+## added in the ready() function or called from the script itself
 ## [codeblock]
 ## class_name_dict["res://path_to_script.gd"] = "Class Name"
 ## [/codeblock]
@@ -136,11 +20,12 @@ var class_name_dict : Dictionary
 
 
 func _enter_tree() -> void:
+	# We are trying to fill the name dictionary as early as possible
 	class_name_dict = get_singletons()
-
+	
 
 func _ready() -> void:
-	d('LogDuck %s ready to go! 🦆' % VERSION)
+	if verbose: d('LogDuck %s ready to go! 🦆' % VERSION)
 
 
 func d(msg, arg1 = null, arg2 = null, arg3 = null, arg4 = null, arg5 = null, arg6 = null):
@@ -199,7 +84,7 @@ func argument_to_string(arg) -> String:
 		return str(arg)
 
 
-func extract_class_name_from_path(path):
+func extract_class_name_from_gdscript(path):
 	var file = FileAccess.open(path, FileAccess.READ)
 	var _class : String
 	if file:
@@ -242,14 +127,15 @@ func _output(level : LogLevel, msg, arg1, arg2, arg3, arg4, arg5, arg6):
 	if class_name_dict.has(frame[0]):
 		_class = class_name_dict[frame[0]]
 	else:
-		_class = extract_class_name_from_path(frame[0])
+		# Check if script is .cs or .gd
+		_class = extract_class_name_from_gdscript(frame[0])
 	
 	var _message : String
 	var _arguments : String
 	
 	var args_original : Array[Variant] = [arg1, arg2, arg3, arg4, arg5, arg6]
 	
-	if not msg is String:
+	if msg is not String:
 		args_original.push_front(msg)
 	else:
 		_message += msg
@@ -273,7 +159,7 @@ func _output(level : LogLevel, msg, arg1, arg2, arg3, arg4, arg5, arg6):
 		SeperatorType.PIPE:
 			seperator = " | "
 	
-	for arg_string in args_processed:
+	for arg_string : String in args_processed:
 		_arguments += seperator + arg_string
 	
 	var msg_plain 
@@ -332,7 +218,7 @@ func output_messages(level, msg_plain, msg_rich, full_stack_rich, stack_frame_ri
 						print(stack_frame_plain)
 					
 		LogLevel.WARN:
-			if enable_debugger_warnings:
+			if enable_warnings_in_debugger:
 				if OS.has_feature("editor"):
 					push_warning(msg_plain)
 			if enable_console_warnings:
@@ -362,6 +248,13 @@ func output_messages(level, msg_plain, msg_rich, full_stack_rich, stack_frame_ri
 						print(full_stack_plain)
 					elif show_stack_frame_debug:
 						print(stack_frame_plain)
+
+
+## Call to overwrite / set a custom identifier for the output name
+func register_class_name(_class_name : String) -> void:
+	var path = stack_frame(2)['source']
+	if verbose: d("Registering class '%s' with path %s" % [path, _class_name])
+	class_name_dict[path] = _class_name
 
 
 #region Helper Functions
---
+++ addons/LogDuck/LogDuckSettings.gd
@@ -0,0 +1,131 @@
+extends Node
+class_name LogDuckSettings
+
+## Use LogDuck.d(), .e(), or .w() for logging debug, error, or warning messages.
+## Supports up to 6 arguments (or 7, if not starting with a string message).
+## Ensure LogDuck is high in the autoload script order to avoid null errors.
+
+## Show the stack frame that called the log in Debug messages.
+var show_stack_frame_debug = false
+
+## Show the entire stack frame that called the Log in Debug messages.
+var show_full_stack_debug = false
+
+## Show the last stack frame that called the log in Warning messages.
+var show_stack_frame_warning = false
+
+## Show the entire stack frame that called the Log in Warning messages.
+var show_full_stack_warning = false
+
+## Show the last stack frame that called the log in Error messages.
+var show_stack_frame_error = false
+
+## Show the entire stack frame that called the Log in Error messages.
+## Note: If 
+var show_full_stack_error = false
+
+
+## The text-prefix to be added to the beginning of Error messages
+var prefix_error = "[ERROR] "
+## The text-prefix to be added to the beginning of Warning messages
+var prefix_warning = "[WARNING] "
+## The text-prefix to be added to the beginning of Debug messages
+var preface_debug = ""
+
+
+## The output format of (plain) console messages
+## Order of placeholders: Prefix, Class Name, Message/Argument, Extra Arguments
+var console_output_format = "%s(%s) %s %s"
+
+## The format of the stack frame line when enabled.
+## Order of placeholders: Script path, line number, function
+var console_format_stack_frame = "%s:%s -> %s"
+
+## The format of the stack frame line when enabled.
+## Stack is output as a single string
+var console_format_full_stack = "%s"
+
+## Show Debug messages in the console
+var enable_console_debug := true
+
+## Show warnings in the console
+var enable_console_warnings := true
+
+## Push warnings to the Godot Debugger
+## (Only works in editor)
+var enable_warnings_in_debugger := true
+
+## Show errors in the console
+var enable_console_errors := true
+
+## Push errors to the Godot Debugger
+## (Only works in editor)
+var enable_errors_in_debugger := true
+
+## Enable / disable asserts to pause the game when an error gets logged
+## Note: Only works in editor or debug builds
+var throw_exception_on_error := false
+
+#var SHOW_TIME : bool = true
+#var SHOW_SECONDS_AFTER_LAUNCH : bool = true
+
+## Select, which type of seperator to use for the arguments
+var seperator : SeperatorType = SeperatorType.COMMA
+
+
+
+## Settings to customize print_rich() output. Please note, that if you remove
+## a %s-placeholder, you will need to remove the arguments when formatting the string.
+
+## Enable to customize your ouput with BBCode
+var print_rich : bool = true
+
+## Set true if you don't want to parse BB Code sent in arguments
+## Can be useful for chat messages, or if you output RichLabelText content
+var escape_bbcode_in_arguments : bool = true
+
+
+## Format to be applied to rich console output for stack frame
+var rich_console_format_stack_frame = "[i]%s:%s -> %s[/i]"
+
+## Set true if you don't want to parse BB Code sent in arguments
+## Can be useful for chat messages, or if you output RichLabelText content
+var rich_console_format_full_stack = "[i]%s[/i]"
+
+# (1) Prefix (see above) (2) Class Name
+var rich_prefix_error = "[b]%s[/b] [b](%s)[/b] " # Uses prefix_error for 1st %s
+var rich_msg_error = "%s"
+var rich_args_error = "[i]%s[/i]"
+
+var rich_prefix_warning = "[b]%s[/b] [b](%s)[/b] " # Uses prefix_warning for 1st %s
+var rich_msg_warning = "%s"
+var rich_args_warning = "[i]%s[/i]"
+
+var rich_preface_debug = "[b]%s[/b][b](%s)[/b] " # Uses preface_debug for 1st %s
+var rich_msg_debug = "%s"
+var rich_args_debug = "[i]%s[/i]"
+
+# Add BBCode tags that will apply to the entire output message
+var rich_format_debug = "[color=white]%s%s %s[/color]" % [rich_preface_debug, rich_msg_debug, rich_args_debug]
+var rich_format_warn = "[color=yellow]%s%s %s[/color]" % [rich_prefix_warning, rich_msg_warning, rich_args_warning]
+var rich_format_error = "[color=red]%s%s %s[/color]" % [rich_prefix_error, rich_msg_error, rich_args_error]
+
+## Outputs some of the internal workings into the log
+var verbose := true
+
+####################################
+## Nothing to set below this line ##
+####################################
+
+## Used to identify the log level of the message throughout the script
+enum LogLevel {
+	DEBUG,
+	WARN,
+	ERROR,
+}
+
+enum SeperatorType {
+	SPACE,
+	COMMA,
+	PIPE, # |
+}
